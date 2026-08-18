15f11868ea9150db56f91962fca9693d18b180dc
redu <nara1415karol@gmail.com>
2026-08-18 16:58:07 +0300

    modifying the bild.sh file

--- backend/build.sh
+++ backend/build.sh
@@ -10,5 +10,5 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
-python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('TRUNCATE TABLE products_category RESTART IDENTITY CASCADE;')"
+
 python manage.py createsuperuser --noinput || true
